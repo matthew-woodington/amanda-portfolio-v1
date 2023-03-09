25e06a8723e1373da360691739c5ad8d37540b7a
matthew-woodington <smwoodington@gmail.com>
2023-03-09 17:35:27 -0500

    animate art content

--- src/Pages/Art/Art.jsx
+++ src/Pages/Art/Art.jsx
@@ -1,5 +1,5 @@
 import "./art.css"
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import art1 from '../../assets/images/art1.jpeg'
 import art2 from '../../assets/images/art2.jpeg'
 import art3 from '../../assets/images/art3.jpeg'
@@ -39,19 +39,37 @@ const ART_LIST = [
 function Art() {
   const [activeIndex, setActiveIndex] = useState(0)
   const [display, setDisplay] = useState(ART_LIST[activeIndex])
+  const [animate, setAnimate] = useState(false)
 
   useEffect(() => {
     setDisplay(ART_LIST[activeIndex])
   }, [activeIndex])
 
   const changeDisplay = (index) => {
+    if (index == activeIndex) {
+      return
+    }
+
+    setAnimate(true)
+    setTimeout(updateDisplayInfo, 600, index)
+  }
+
+  const updateDisplayInfo = (index) => {
+    setAnimate(false)
+
+    const previous = activeIndex
+    const activeBtn = document.getElementById(`dispbtn ${index}`)
+    const previousBtn = document.getElementById(`dispbtn ${previous}`)
+
     setActiveIndex(index)
-    console.log(index)
+
+    activeBtn.classList.add('active')
+    previousBtn.classList.remove('active')
   }
 
   return (
     <div id='art' className='art'>
-      <div className="art-display">
+      <div className={animate ? "art-display animate" : "art-display"}>
         <div className="img-container">
           <img src={display.image} alt="artwork" className="display-img" />
           <h2 className="display-title">{display.title}</h2>
@@ -61,10 +79,10 @@ function Art() {
           <span className="info-number">{display.number}</span>
         </div>
         <div className="display-buttons">
-          <button className="toggle-btn" onClick={(e) => changeDisplay(0)}><div className="btn-icon" /></button>
-          <button className="toggle-btn" onClick={(e) => changeDisplay(1)}><div className="btn-icon" /></button>
-          <button className="toggle-btn" onClick={(e) => changeDisplay(2)}><div className="btn-icon" /></button>
-          <button className="toggle-btn" onClick={(e) => changeDisplay(3)}><div className="btn-icon" /></button>
+          <button id="dispbtn 0" className="toggle-btn active" onClick={() => changeDisplay(0)}><div className="btn-icon" /></button>
+          <button id="dispbtn 1" className="toggle-btn" onClick={() => changeDisplay(1)}><div className="btn-icon" /></button>
+          <button id="dispbtn 2" className="toggle-btn" onClick={() => changeDisplay(2)}><div className="btn-icon" /></button>
+          <button id="dispbtn 3" className="toggle-btn" onClick={() => changeDisplay(3)}><div className="btn-icon" /></button>
         </div>
       </div>
     </div>
